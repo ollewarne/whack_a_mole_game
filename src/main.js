3596decc14a56a0ae6656ab84e36dd51c54717d5
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-11-20 11:18:30 +0100

    import game class, save ui, init game, add eventlisteners for start and reset buttons

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { Game } from "./modules/Game.js";
+
+const gameUi = {
+    boardElement: document.querySelector("#board"),
+    scoreElement: document.querySelector("#score"),
+    timerElement: document.querySelector("#timer"),
+    missesElement: document.querySelector("#misses"),
+    startButton: document.querySelector("#start-button"),
+    resetButton: document.querySelector("#reset-button"),
+}
+
+const game = new Game(gameUi);
+game.initGame();
+
+
+gameUi.startButton.addEventListener('click', () => game.start());
+gameUi.startButton.addEventListener('click', () => game.reset());
